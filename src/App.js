0add32079be89cd9686c46a71b419d9e6653a9fc
ProgrammerShahJalal <you@example.com>
2023-02-19 20:45:44 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Home from './Components/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import QuranPage from './Components/QuranPage'
+import loadingImg from './Components/images/loading.gif';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   
+  if(isLoading) {
+    return (
+      <div id='loading'>
+        <img src={loadingImg} alt='loading animaiton' />
+      </div>
+    )
+  }
+
   return (
     <BrowserRouter>
       <Routes>
