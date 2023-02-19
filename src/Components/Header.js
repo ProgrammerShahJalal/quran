e7c505c3fbf50785eac92e15c73c4b1cfb4fcaf6
ProgrammerShahJalal <you@example.com>
2023-02-19 09:36:40 +0600

    quran

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,9 +5,6 @@ import '../App.css'
 const Header = () => {
   return (
     <div className='hero'>
-    <div className="my-container">
-
-    </div>
    <img src={alquranImg} alt='bismillah' />
     </div>
   )
